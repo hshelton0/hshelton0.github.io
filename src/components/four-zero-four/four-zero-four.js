614a32bbf2cac9ade2951f3958442b85cf987c58
hshelton0 <hshelton@gmail.com>
2025-05-29 16:27:12 -0700

    site updates

--- src/components/four-zero-four/four-zero-four.js
+++ src/components/four-zero-four/four-zero-four.js
@@ -4,10 +4,10 @@ import "./four-zero-four.scss"
 const FourZeroFour = () => (
   <>
     <body className="not-found">
-      <div className="name">Kavin Varnan</div>
+      <div className="name">nope</div>
       <img src="../images/emojis/thinking-face.png" alt=""/>
       <h1>Hmmm...</h1>
-      <h2>Looks like there's nothing here.<br/>Maybe you should go back <a href="/">home</a>.</h2>
+      <h2>Looks like there's nothing here.<br/>Maybe we go back <a href="/">home</a>.</h2>
     </body>
   </>
 )
